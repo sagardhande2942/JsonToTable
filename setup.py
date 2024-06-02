a175feb18326a2422de4aa9200881fa28be07ae9
sagardhande <dhandesagar78@gmail.com>
2024-06-02 11:49:44 +0530

    Json X Table V2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = '0.0.1' 
+VERSION = '0.0.2' 
 DESCRIPTION = 'Convert your json dump files with a certain format to HTML tables in a fast and efficient way'
 LONG_DESCRIPTION = '''Convert your json dump files with a certain format to HTML tables in a fast and efficient way.\nYou may want to override the functions in order to match any format that you want, with specific customizable column values.'''
 
@@ -11,8 +11,8 @@ setup(
     author_email="dhandesagar78@gmail.com",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    packages=find_packages("jsoncomment"),
-    install_requires=[],
+    packages=find_packages(),
+    install_requires=["jsoncomment", "pathlib"],
     keywords=['python3', 'Json_to_HTML'],
     classifiers= [
         "Development Status :: 3 - Alpha",
