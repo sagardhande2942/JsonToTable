83f05bc1685761829b62dac0ceb691e97ff188a1
sagardhande <dhandesagar78@gmail.com>
2024-06-03 08:52:43 +0530

    Fixed issue where not all rows were being converted

--- json_x_table/table_parser.py
+++ json_x_table/table_parser.py
@@ -18,9 +18,9 @@ def parse_table(file_path: pathlib.Path) -> Table:
   json_parser = JsonComment(json)
   data = json_parser.loads(file_path.read_text())
   table = Table()
-  for row, data in data.items():
+  for row, row_data in data.items():
     rows = []
-    for column, value in data.items():
+    for column, value in row_data.items():
       if column not in table: 
         table.add_column(column, types_mapping[value["type"]])
       rows.append(value["value"])
